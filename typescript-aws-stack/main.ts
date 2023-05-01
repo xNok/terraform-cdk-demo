900170d7c33fdf9bb679fb182547d49b3ebda30e
Alexandre Couedelo <nokwebspace@gmail.com>
2023-05-01 18:48:05 +0000

    feat: add github

--- typescript-aws-stack/main.ts
+++ typescript-aws-stack/main.ts
@@ -1,22 +1,39 @@
 import { Construct } from "constructs";
 import { App, TerraformStack, CloudBackend, NamedCloudWorkspace, TerraformOutput } from "cdktf";
 import { AwsProvider } from "@cdktf/provider-aws/lib/provider";
+import { GithubProvider } from "@cdktf/provider-github/lib/provider"
 import { Instance } from "@cdktf/provider-aws/lib/instance";
+import { ActionsVariable } from "@cdktf/provider-github/lib/actions-variable"
+import { DataGithubRepository } from "@cdktf/provider-github/lib/data-github-repository"
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
-    // define resources here
     new AwsProvider(this, "AWS", {
       region: "us-west-1",
     });
 
+    new GithubProvider(this, "GitHub", {})
+
+    // AWS
     const ec2Instance = new Instance(this, "compute", {
       ami: "ami-01456a894f71116f2",
       instanceType: "t2.micro",
     });
 
+    // GITHUB
+    const repo = new DataGithubRepository(this, "repo", {
+      fullName:  "xNok/terraform-cdk-demo",
+    })
+
+    new ActionsVariable(this, "public_ip", {
+      repository: repo.name,
+      value: ec2Instance.publicIp,
+      variableName:"PUBLIC_IP"
+    })
+
+    // OUTPUT
     new TerraformOutput(this, "public_ip", {
       value: ec2Instance.publicIp,
     });
