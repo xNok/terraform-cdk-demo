b4cfdcc1af295260af726eec48f52dc0cef3c14b
Alexandre Couedelo <nokwebspace@gmail.com>
2023-05-01 18:27:15 +0000

    feat: create instance

--- Makefile
+++ Makefile
@@ -5,4 +5,13 @@ check:
 	cdktf --version
 
 init:
+	cd typescript-aws-stack; \
 	cdktf init --template="typescript" --providers="aws@~>4.65"
+
+deploy:
+	cd typescript-aws-stack; \
+	cdktf deploy
+
+destroy:
+	cd typescript-aws-stack; \
+	cdktf destroy
--- typescript-aws-stack/main.ts
+++ typescript-aws-stack/main.ts
@@ -1,11 +1,25 @@
 import { Construct } from "constructs";
-import { App, TerraformStack, CloudBackend, NamedCloudWorkspace } from "cdktf";
+import { App, TerraformStack, CloudBackend, NamedCloudWorkspace, TerraformOutput } from "cdktf";
+import { AwsProvider } from "@cdktf/provider-aws/lib/provider";
+import { Instance } from "@cdktf/provider-aws/lib/instance";
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
     // define resources here
+    new AwsProvider(this, "AWS", {
+      region: "us-west-1",
+    });
+
+    const ec2Instance = new Instance(this, "compute", {
+      ami: "ami-01456a894f71116f2",
+      instanceType: "t2.micro",
+    });
+
+    new TerraformOutput(this, "public_ip", {
+      value: ec2Instance.publicIp,
+    });
   }
 }
 
