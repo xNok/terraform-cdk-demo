b2df29157bbcd7fd4a4e8ef0b724148fe7c8af9f
Alexandre Couedelo <a.couedelo@efg.gg>
2023-06-11 13:53:05 +0000

    feat: featch api config

--- typescript-aws-stack/fetchConfig.test.ts
+++ typescript-aws-stack/fetchConfig.test.ts
@@ -1,4 +1,14 @@
-import { fetchConfig } from "./fetchConfig";
+import { fetchConfig, fetchConfigApi } from "./fetchConfig";
+import type { FetchMockStatic } from 'fetch-mock';
+import fetch from 'node-fetch';
+import 'fetch-mock-jest';
+
+jest.mock(
+    'node-fetch', 
+    () => require('fetch-mock-jest').sandbox(),
+);
+
+const fetchMock = (fetch as unknown) as FetchMockStatic;
 
 test('fetch config as yaml', () => {
     const config = fetchConfig("typescript-aws-stack")
@@ -8,3 +18,16 @@ test('fetch config as yaml', () => {
         "repoId": "xNok/terraform-cdk-demo"
     })
 });
+
+test('fetch config as api', () => {
+    fetchMock.reset()
+    fetchMock.get('https://api.example.com/typescript-aws-stack', { value: 1234 });
+
+    fetchConfigApi("typescript-aws-stack")
+    .then(config => expect(config).toStrictEqual({
+        "imageId": "ami-01456a894f71116f2",
+        "imageSize": "t2.micro",
+        "repoId": "xNok/terraform-cdk-demo"
+    }))
+});
+
--- typescript-aws-stack/fetchConfig.ts
+++ typescript-aws-stack/fetchConfig.ts
@@ -1,5 +1,6 @@
 import {load} from 'js-yaml';
 import {readFileSync} from 'fs';
+import fetch from 'node-fetch';
 
 interface Config {
     imageId: string;
@@ -11,3 +12,14 @@ export function fetchConfig(stack: string): Config {
     const yaml = load(readFileSync(stack + '.yaml', "utf8")) as Config;
     return yaml;
 }
+
+export async function fetchConfigApi(stack: string): Promise<Config> {
+    const response = await fetch('https://api.example.com/' + stack);
+    const config = await response.json() as Config;
+
+    return config
+}
+
+
+
+
